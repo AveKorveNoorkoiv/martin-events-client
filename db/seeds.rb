6a483c1337bfe837f189820731edf7fc1b5d6926
AveKorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-09-29 12:05:45 +0300

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Category.create([{name: 'Muusika'}, {name: 'Teater' }, {name: 'Sport' }, {name: 
 
 #Adding one event
 Event.create({
-   name: "Lorem ipsum",
+   name: "Laula!",
    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    price: 3000,
@@ -26,7 +26,7 @@ Event.create({
    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    price: 30,
    date: Date.today + 3.weeks,
-   location: "Tallinn",
+   location: "Uugametsa",
    category: Category.where(name: "Sport").first
 })
 Event.create({
@@ -35,16 +35,67 @@ Event.create({
    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    price: 300,
    date: Date.today + 3.years,
-   location: "Tallinn",
+   location: "Narva",
    category: Category.where(name: "Festival").first
 })
 
 Event.create({
-   name: "Tsapai ja pustota",
+   name: "Kingime midagi ägedat",
    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    price: 300,
    date: Date.today + 3.days,
-   location: "Tallinn",
-   category: Category.where(name: "Teater").first
+   location: "Eesti",
+   category: Category.where(name: "EV100").first
 })
+
+Event.create({
+   name: "Võta või jäta",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 300,
+   date: Date.today + 3.days,
+   location: "Forum Cinemas",
+   category: Category.where(name: "Film").first
+})
+
+Event.create({
+   name: "Lastele lastest",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 300,
+   date: Date.today + 3.days,
+   location: "Võru",
+   category: Category.where(name: "Kogupere").first
+})
+
+Event.create({
+   name: "Ticket system bro",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 300,
+   date: Date.today + 2.months + 3.days,,
+   location: "Worldvide",
+   category: Category.where(name: "Kinkekaardid").first
+})
+
+Event.create({
+   name: "Ehitame roboti",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 300,
+   date: Date.today - 1.years,
+   location: "Tartu",
+   category: Category.where(name: "Varia").first
+})
+
+Event.create({
+   name: "Seenelkäik",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 300,
+   date: Date.new(2019, 07, 26),
+   location: "Valga",
+   category: Category.where(name: "Näitus").first
+})
+
