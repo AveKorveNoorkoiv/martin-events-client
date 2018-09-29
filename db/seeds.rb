4fd62e0e0d7e00e45791586ab4f43e001f116226
AveKorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-09-29 14:16:53 +0300

    Added and changed some marks in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,7 +74,7 @@ Event.create({
    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    price: 300,
-   date: Date.today + 2.months + 3.days,,
+   date: Date.today + 2.months + 3.days,
    location: "Worldvide",
    category: Category.where(name: "Kinkekaardid").first
 })
@@ -99,4 +99,4 @@ Event.create({
    category: Category.where(name: "Näitus").first
 })
 
-User.create({email: 'ave.korve-noorkoiv@khk.ee', password: ‘123123’}).confirm
+User.create({email: 'ave.korve-noorkoiv@khk.ee', password: '123123' }).confirm
