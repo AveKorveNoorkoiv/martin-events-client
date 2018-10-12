f6c5552ab270c501c59369f4c96e0056a6a0360a
AveKorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-10-12 14:38:18 +0300

    Added payment.rb

---
+++ app/controllers/concerns/payment.rb
@@ -0,0 +1,18 @@
+class Payment
+
+   def request
+       uri = URI('http://vso17.cariba.ee/payments.json')
+       http = Net::HTTP.new(uri.host, uri.port)
+       req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+       req.body = {
+       "amount": 120,
+       "recipient_name": "AKN",
+       "sender_account": "ave@mail.ee",
+       "recipient_account": 1ct77,
+       "description": "Midagimisagi!",
+       "token": "2899cfab862af6858bb8c2eb174da0fe"
+       }.to_json
+       res = http.request(req)
+   end
+
+end
